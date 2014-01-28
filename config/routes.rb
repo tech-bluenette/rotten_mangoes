de0ada75c176e62a5e13ae6dc12d3da6ac41d853
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-28 03:27:10 +0000

    finished step 13

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ RottenMangoes::Application.routes.draw do
   get "sessions/new"
   get "sessions/create"
   get "users/new"
-  get "users/create"
- resources :movies
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  
  resources :users, only: [:new, :create]
- resources :sessions, only: [:new, :create]
  resources :sessions, only: [:new, :create, :destroy]
 end
