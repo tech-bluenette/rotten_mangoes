5a7b5d1aefe6f3197608210416335e63228f6ddb
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-28 01:35:29 +0000

    finished step 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+ def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id # auto log in
       redirect_to movies_path
     else
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127223154) do
+ActiveRecord::Schema.define(version: 20140128004723) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140127223154) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
